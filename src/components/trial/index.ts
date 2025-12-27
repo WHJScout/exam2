f9713752f7401990be95cca5ba588581ce0e4a79
WHJScout <270041283@qq.com>
2025-12-27 16:04:38 +0800

    Delete src directory

--- src/components/trial/index.ts
+++
@@ -1,4 +0,0 @@
-export { CountdownTimer } from './CountdownTimer';
-export { GuessPhase } from './GuessPhase';
-export { FeedbackPhase } from './FeedbackPhase';
-export { ReviewPhase } from './ReviewPhase';
